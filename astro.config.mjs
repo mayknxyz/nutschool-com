abd41fc99ccbcdd1b7b83fe0d485040c1f6d1b25
Mike Navales <user@example.com>
2025-04-08 16:05:16 +0800

    update site info

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://nutschool.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "NutSchool";
+export const SITE_DESCRIPTION = "Raise kids to be great adults!";
